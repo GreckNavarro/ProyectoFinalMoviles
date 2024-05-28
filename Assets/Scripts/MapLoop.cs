b81cb2b39a93eceeffa36dedefe1267857cd5c6f
KyAMogollon <adhair.mogollon@tecsup.edu.pe>
2024-05-28 16:48:25 -0500

    Mapa

---
+++ Assets/Scripts/MapLoop.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLoop : MonoBehaviour
+{
+    private GameObject[,] Map;
+    private GameObject mapToInstance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Map = new GameObject[3,3];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
